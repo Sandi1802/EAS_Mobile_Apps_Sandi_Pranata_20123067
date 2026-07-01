b61df3c7c26275cae28d6a1aaa2a6e70a5f6b20c
Sandi1802 <sandipranata1802@gmail.com>
2026-07-01 14:49:00 +0700

    feat: create article entity

--- lib/domain/entities/article.dart
+++ lib/domain/entities/article.dart
@@ -3,11 +3,13 @@ class Article {
   final String description;
   final String urlToImage;
   final String publishedAt;
+  final String url; // URL ke artikel asli untuk dibuka di browser
 
   const Article({
     required this.title,
     required this.description,
     required this.urlToImage,
     required this.publishedAt,
+    required this.url,
   });
 }
