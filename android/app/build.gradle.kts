7b0f32c0640f4491e60db84a1fe3338ff2df4e41
Sandi1802 <sandipranata1802@gmail.com>
2026-07-02 09:20:00 +0700

    fix: setup android app gradle dart-defines

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Base64
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,9 +7,21 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val dartEnvironmentVariables = mutableMapOf<String, String>()
+if (project.hasProperty("dart-defines")) {
+    val dartDefines = project.property("dart-defines") as String
+    dartDefines.split(",").forEach {
+        val decoded = String(Base64.getDecoder().decode(it), Charsets.UTF_8)
+        val split = decoded.split("=", limit = 2)
+        if (split.size == 2) {
+            dartEnvironmentVariables[split[0]] = split[1]
+        }
+    }
+}
+
 android {
     namespace = "com.example.mobile_apps"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
@@ -15,13 +29,20 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+    kotlin {
+        jvmToolchain(17)
     }
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        val flavor = dartEnvironmentVariables["FLAVOR"] ?: "dev"
         applicationId = "com.example.mobile_apps"
+        if (flavor == "dev") {
+            applicationIdSuffix = ".dev"
+        }
+        
+        val appName = dartEnvironmentVariables["APP_NAME"] ?: "DigiNews"
+        manifestPlaceholders["appName"] = appName
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
